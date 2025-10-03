ba465c87d3d596bd00461d16490af854079c8c76
Sabbir Ahmed Shonon <sabbirahmedshonon@gmail.com>
2025-10-03 23:48:26 +0600

    Odoo Dashboard Partially finished

--- app/components/ErgoDashboard.jsx
+++ app/components/ErgoDashboard.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import React from "react";
-
+import { GoAlert } from "react-icons/go";
+import Experience from "./Experience";
 const ErgoDashboard = () => {
   return (
     <div className="max-w-7xl w-full mx-auto ">
       {/* Banner */}
-      <div class="mt-10 bg-[url('/book.jpg')] bg-cover bg-center h-78 px-3">
+      <div className="mt-10 bg-[url('/book.jpg')] bg-cover bg-center h-78 px-3">
         <h1 className="font-bold text-3xl text-white p-8 ml-7">
           EXPERTS IN DRUPAL,ODOO, AND ENTERPRICE SOLUTIONS
         </h1>
@@ -16,6 +17,66 @@ const ErgoDashboard = () => {
         <button className="bg-green-700 text-white p-4 ml-15 cursor-pointer">
           EXPLORE WORK
         </button>
+      </div>
+      {/* 2nd part */}
+      <Experience />
+      <div className="my-10 bg-[url('/greenfield.jpg')] bg-cover bg-center h-78 p-3">
+        <div className="flex justify-between text-white">
+          <div className="text-3xl font-bold mt-20">
+            <p>Ergo Ventures is a </p>
+            <p>Certified Odoo Silver</p>
+            <p>Partner</p>
+          </div>
+          <div className="mr-7">
+            <p className="text-4xl font-bold mt-20">Odoo</p>
+            <button className="bg-gray-400 p-2 rounded cursor-pointer">
+              Silver Partner
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-2xl font-bold">SELECTED WORKS</p>
+        <div>
+          <div className="flex mt-7 gap-4  max-w-5xl mx-auto w-full">
+            <div>
+              <div className="relative min-h-60 h-full min-w-lg w-full ">
+                <Image src="/phone1.jpg" alt="phone1 loading" fill />
+              </div>
+              <p className="font-bold text-black">SELF CARE APP FOR A TELCO </p>
+            </div>
+
+            <div>
+              <div className="relative min-h-60 h-full min-w-lg w-full">
+                <Image src="/phone2.jpg" alt="phone1 loading" fill />
+              </div>
+              <p className="font-bold text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur{" "}
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-7 gap-4  max-w-5xl mx-auto w-full">
+            <div>
+              <div className="relative min-h-60 h-full min-w-lg w-full ">
+                <Image src="/phone3.jpg" alt="phone1 loading" fill />
+              </div>
+              <p className="font-bold text-black">SELF CARE APP FOR A TELCO </p>
+            </div>
+
+            <div>
+              <div className="relative min-h-60 h-full min-w-lg w-full">
+                <Image src="/phone4.jpg" alt="phone1 loading" fill />
+              </div>
+              <p className="font-bold text-black">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consectetur{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex"></div>
       </div>
     </div>
   );
---
+++ app/components/Experience.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { GoAlert } from "react-icons/go";
+const Experience = () => {
+  return (
+    <div>
+      <div className="mt-10 bg-gray-200 w-full p-10 ">
+        <p className="text-3xl ">CRAFTING SCALABLE DIGITAL</p>
+        <p className="text-3xl">EXPERIENCES</p>
+        <div className="flex gap-1.5">
+          <div className="mt-5 bg-white p-7 ">
+            <GoAlert size={40} className="text-emerald-500 " />
+
+            <p className="mt-3 font-bold">TECH STRATEGY CONSULTING &</p>
+            <p className="font-bold"> RESOURCE ALLOCATION</p>
+            <p className="font-extralight mt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              sint temporibus nulla officia deserunt? Nemo ut ipsam recusandae
+              qui consequatur. Eos nam corporis nobis temporibus maxime vitae
+              libero suscipit illo.
+            </p>
+          </div>
+
+          <div className="mt-5 bg-white p-7 ">
+            <GoAlert size={40} className="text-emerald-500 " />
+
+            <p className="mt-3 font-bold">TECH STRATEGY CONSULTING &</p>
+            <p className="font-bold"> RESOURCE ALLOCATION</p>
+            <p className="font-extralight mt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              sint temporibus nulla officia deserunt? Nemo ut ipsam recusandae
+              qui consequatur. Eos nam corporis nobis temporibus maxime vitae
+              libero suscipit illo.
+            </p>
+          </div>
+
+          <div className="mt-5 bg-white p-7 ">
+            <GoAlert size={40} className="text-emerald-500 " />
+
+            <p className="mt-3 font-bold">TECH STRATEGY CONSULTING &</p>
+            <p className="font-bold"> RESOURCE ALLOCATION</p>
+            <p className="font-extralight mt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+              sint temporibus nulla officia deserunt? Nemo ut ipsam recusandae
+              qui consequatur. Eos nam corporis nobis temporibus maxime vitae
+              libero suscipit illo.
+            </p>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
