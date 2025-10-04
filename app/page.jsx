ac377ac0fad3fac506615af04b1367f653fbb11e
Sabbir Ahmed Shonon <sabbirahmedshonon@gmail.com>
2025-10-05 03:32:20 +0600

    Calculator finish with easy logic

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+
 import Card from "./components/Card";
 import ErgoDashboard from "./components/ErgoDashboard";
 import RecipeCard from "./components/RecipeCard";
 import ShundarbanForm from "./components/ShundarbanForm";
 import Shunderban from "./components/Shunderban";
+import Calculator from "./components/Calculator";
 export default function Home() {
   return (
     <div>
@@ -10,7 +12,8 @@ export default function Home() {
       {/* <RecipeCard /> */}
       {/* <ErgoDashboard /> */}
       {/* <Shunderban/> */}
-      <ShundarbanForm/>
+      {/* <ShundarbanForm/> */}
+      <Calculator/>
     </div>
   );
 }
