9f918712a9cc5b334845072277228cbf569167fe
misl.serazy <amihasan420@gmail.com>
2025-10-05 23:42:59 +0600

    dynamically customer list added

--- app/components/CustomerList.jsx
+++ app/components/CustomerList.jsx
@@ -1,70 +1,97 @@
 import React from "react";
 import { GoTriangleDown } from "react-icons/go";
 const CustomerList = () => {
+  const customerDetails = [
+    {
+      id: 1,
+      name: "Sabbir Ahmed Shonon",
+      img: "https://avatar.iran.liara.run/public",
+      email: "amiShonon@gmail.com",
+      phoneNumber: "+01883266793",
+      gender: "Male",
+    },
+    {
+      id: 2,
+      name: "Tanvir Hasan Serazy",
+      img: "https://avatar.iran.liara.run/public",
+      email: "amiHasan@gmail.com",
+      phoneNumber: "+01883266793",
+      gender: "Male",
+    },
+    {
+      id: 3,
+      name: "Sowat Bhai",
+      img: "https://avatar.iran.liara.run/public",
+      email: "amiSowat@gmail.com",
+      phoneNumber: "+01883266793",
+      gender: "Male",
+    },
+    {
+      id: 4,
+      name: "Romel Bhai",
+      img: "https://avatar.iran.liara.run/public",
+      email: "amiRomel@gmail.com",
+      phoneNumber: "+01883266793",
+      gender: "female",
+    },
+  ];
+
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="bg-gray-50 ">
+      <div className="overflow-x-auto">
         <p className="py-10 text-center text-3xl font-bold">Customer List</p>
-        <div className="flex justify-between max-w-7xl mx-auto">
-          {/* Titles */}
+        {/* Titles */}
+        <div className="flex justify-between max-w-7xl mx-auto overflow-x-auto hover:bg-blue-400">
           <div className="flex gap-1">
-            <div>
-              <p>Name</p>
-            </div>
+            <p>Name</p>
             <div>
               <GoTriangleDown className="mt-1 " />
             </div>
           </div>
 
           <div className="flex">
-            <div>
-              <p>Email</p>
-            </div>
+            <p>Email</p>
             <div>
               <GoTriangleDown className="mt-1 ml-1" />
             </div>
           </div>
 
           <div className="flex">
-            <div>
-              <p>Phone Number</p>
-            </div>
+            <p>Phone Number</p>
             <div>
               <GoTriangleDown className="mt-1 ml-1" />
             </div>
           </div>
 
           <div className="flex">
-            <div>
-              <p>Gender</p>
-            </div>
+            <p>Gender</p>
             <div>
               <GoTriangleDown className="mt-1 ml-1" />
             </div>
           </div>
         </div>
+        {/* content */}
 
-        <div className="flex justify-between">
-          <div className="bg-white rounded-2xl ">
-            <div className="flex mt-5 space-x-3 items-center min-w-16">
-              <img
-                className="max-h-7"
-                src="https://avatar.iran.liara.run/public"
-              />
-              <p className="bg-white rounded-2xl ">John Deo</p>
+        {customerDetails?.map((data) => (
+          <div key={data.id} className="flex justify-between mt-5">
+            <div className="rounded-2xl min-w-52">
+              <div className="flex space-x-3 min-w-16">
+                <img className="max-h-7" src={data.img} alt="avatar" />
+                <p className=" rounded-2xl ">{data?.name}</p>
+              </div>
             </div>
+            <div className=" rounded-2xl min-w-56">
+              <p>{data?.email}</p>
+            </div>
+            <div className=" rounded-2xl ">
+              <p>{data?.phoneNumber}</p>
+            </div>
+            <div className=" rounded-2xl min-w-10"></div>
+            <button className="bg-blue-200 text-blue-800 px-3 rounded-2xl max-h-7  text-center h-full">
+              {data?.gender}
+            </button>
           </div>
-          <div className="bg-white rounded-2xl ">
-            <p>abc@gmail.com</p>
-          </div>
-          <div className="bg-white rounded-2xl ">
-            <p>abc@gmail.com</p>
-          </div>
-          <div className="bg-white rounded-2xl "></div>
-          <button className="bg-blue-200 text-blue-800 px-3 rounded-2xl max-h-7  text-center h-full">
-            Male
-          </button>
-        </div>
+        ))}
       </div>
     </div>
   );
